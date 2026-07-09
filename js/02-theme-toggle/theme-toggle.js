55dab8b6f3da0881fe35069ce8b633eaea498bd2
Tomas Gic <gic.tomas@gmail.com>
2026-07-09 11:04:31 +0200

    feat(theme-toggle): add a click counter below the theme button

--- js/02-theme-toggle/theme-toggle.js
+++ js/02-theme-toggle/theme-toggle.js
@@ -1,6 +1,15 @@
 const themeButton = document.querySelector("#theme-btn")
+const clicksCountParagraph = document.querySelector("#click-counter")
+const rootElement = document.documentElement
+let clicksCount = 0
 
 themeButton.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode")
 })
+
+rootElement.addEventListener("click", () => {
+    clicksCount += 1
+    clicksCountParagraph.textContent = `Clicks: ${clicksCount}`
+})
+
 

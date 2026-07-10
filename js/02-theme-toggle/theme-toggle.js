1f4b6e8de8881a8fed1d7996fbd3107d328ed2cb
Tomas Gic <gic.tomas@gmail.com>
2026-07-10 14:49:07 +0200

    fix(theme-toggle): fix the reset button functionality which was incorrectly resetting the counter value

--- js/02-theme-toggle/theme-toggle.js
+++ js/02-theme-toggle/theme-toggle.js
@@ -1,10 +1,18 @@
+const rootElement = document.documentElement
 const themeButton = document.querySelector("#theme-btn")
 const clicksCountParagraph = document.querySelector("#click-counter")
-const rootElement = document.documentElement
+const resetButton = document.querySelector("#reset-counter-btn")
 let clicksCount = 0
 
 themeButton.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode")
+   document.body.classList.toggle("dark-mode")
+})
+
+resetButton.addEventListener("click", () => {
+    event.stopPropagation()
+
+    clicksCount = 0
+    clicksCountParagraph.textContent = "Clicks: 0"
 })
 
 rootElement.addEventListener("click", () => {

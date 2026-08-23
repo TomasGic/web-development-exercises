1e15f981717948d653e21c77d7c61c2fe9f80e6c
Tomas Gic <gic.tomas@gmail.com>
2026-08-23 18:50:16 +0200

    feat: add javascript code to preview the button component dynamically with user-selected input

---
+++ design-system/02-component/button.js
@@ -0,0 +1,33 @@
+const previewButton = document.querySelector('#preview-btn')
+const variantSelect = document.querySelector('#variant-select')
+const sizeSelect = document.querySelector('#size-select')
+const disabledToggle = document.querySelector('#disabled-toggle')
+
+disabledToggle.addEventListener('change', (event) => {
+    
+})
+
+function updateButtonPreview() {
+    const variant = variantSelect.value 
+    const size = sizeSelect.value
+
+    previewButton.className = (`btn btn--${variant} btn--${size}`)
+    if (disabledToggle.checked) {
+        previewButton.setAttribute('disabled', '')
+    }
+    else {
+        previewButton.removeAttribute('disabled')
+    }
+}
+
+variantSelect.addEventListener('change', (event) => {
+    updateButtonPreview()
+})
+sizeSelect.addEventListener('change', (event) => {
+    updateButtonPreview()
+})
+
+disabledToggle.addEventListener('change', (event) => {
+    updateButtonPreview()
+})
+

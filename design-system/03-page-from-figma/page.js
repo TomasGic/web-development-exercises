9a7186397d3e1dba9a5bc33b7fefae643442e476
Tomas Gic <gic.tomas@gmail.com>
2026-08-29 19:03:55 +0200

    feat: add javascript filtering functionality to product cards

---
+++ design-system/03-page-from-figma/page.js
@@ -0,0 +1,48 @@
+const cards = document.querySelectorAll('.product-card')
+const filterButtons = document.querySelectorAll('.filter-btn')
+
+function hideAllCards() {
+    cards.forEach(card => {
+        card.classList.add('hidden')
+    })
+}
+
+function showAllCards() {
+    cards.forEach(card => {
+        card.classList.remove('hidden')
+    })
+}
+
+function filterCardsByCategory(category) {
+    hideAllCards()
+    cards.forEach(card => {
+        const cardCategory = card.dataset.category
+        if (category === cardCategory) {
+            card.classList.remove('hidden')
+        }
+    })
+}
+
+function styleActiveButton(button) {
+    filterButtons.forEach(button => {
+        button.classList.remove('btn--primary')
+        button.classList.add('btn--secondary')
+    })
+    button.classList.add('btn--primary')
+    button.classList.remove('btn--secondary')
+}
+
+filterButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        const clickedButton = event.target
+        const selectedCategory = button.dataset.filter
+        if (selectedCategory === 'all') {
+            showAllCards()
+            styleActiveButton(clickedButton)
+            return
+        }
+        filterCardsByCategory(selectedCategory)
+        styleActiveButton(clickedButton)
+    })
+})
+
